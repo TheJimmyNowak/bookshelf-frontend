e68e8a382d769a5847a9a254846e5081aaf1f9a6
Jolsky <lolskypl@gmail.com>
2021-08-09 21:09:33 +0200

    JD-after-logging-in

---
+++ src/components/Offer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <img src="https://www.testhr.pl/wp-content/uploads/2020/01/logo_1kolor.png"/>
+        <div class="book-data">
+          <div class="book-info">
+            <p class="book-title"> {{name}} </p>
+            <p> {{author}} </p>
+          </div>
+          <a v-bind:href="'/book/' + id" class="buy-button">
+            <span>KUP</span>
+          </a>
+        </div>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: "Offer",
+  props:['name', 'author', 'id']
+  
+}
+</script>
+
+<style scoped>
+
+</style>
